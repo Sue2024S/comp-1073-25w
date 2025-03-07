b8ac6b257f117ad4d0942b108833ef592baa7e24
Susan S. <susan.sotosarmiento@mygeorgian.ca>
2025-03-07 16:07:45 -0500

    Update Car instances in object.js with new details and descriptions

--- lab-2/js/object.js
+++ lab-2/js/object.js
@@ -1,4 +1,4 @@
-const output = document.getElementById('output');
+const output = document.getElementById("output");
 
 function Car(name, category, numModel, color, numCategory, numSeries, year, madeIn, material, minAge, brand, language, numRegister, website) {
     this.name = name;
@@ -21,8 +21,10 @@ function Car(name, category, numModel, color, numCategory, numSeries, year, made
     };
 };
 
-const car1 = new Car("Brick Rides", "Bricking Speed", "128/250", "red, yellow, blue, black and white", "5/5","HKJ89-N7C5", 2021, "Malaysia", "plastic and paper", 3, "Mega", "English, French, Spanish and Russian", "004644/2021", "hotwheels.com"); 
+const car1 = new Car("Custom Cadillac Fleetwood", "Rod Squad", "24/250", "green, black, white, and grey", "1/10","HKW77-N7C5", 2012, "Malaysia", "plastic and paper", 3, "GM", "English, French, Spanish and Russian", "004644/2021", "hotwheels.com"); 
 
 const car2 = new Car("Brick Rides", "Bricking Speed", "128/250", "red, yellow, blue, black and white", "5/5","HKJ89-N7C5", 2021, "Malaysia", "plastic and paper", 3, "Mega", "English, French, Spanish and Russian", "004644/2021", "hotwheels.com"); 
 
-const car3 = new Car("Brick Rides", "Bricking Speed", "128/250", "red, yellow, blue, black and white", "5/5","HKJ89-N7C5", 2021, "Malaysia", "plastic and paper", 3, "Mega", "English, French, Spanish and Russian", "004644/2021", "hotwheels.com"); 
+const car3 = new Car("HW Hot Trucks", "67 Chevy C10", "21/250", "light blue, black and white", "2/10","HYW74-N7C5", 2021, "Malaysia", "plastic and paper", 3, "GM", "English, French, Spanish and Russian", "004644/2021", "hotwheels.com"); 
+
+output.textContent = car1.describe();
